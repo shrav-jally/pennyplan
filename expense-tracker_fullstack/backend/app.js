b881885ac3d92ee30396a865f6a1d929a9a94837
Shravya J <jallepally0.0shravya@gmail.com>
2025-03-25 13:20:46 +0530

    branch delete

--- expense-tracker_fullstack/backend/app.js
+++ expense-tracker_fullstack/backend/app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT
 //middlewares
 app.use(express.json())
 app.use(cors())
-
+//routes
 //routes
 readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
